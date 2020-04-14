5696dba36440a53a00181e29e585dd7b473ab005
Dikizeko-Oliveira <dikezekodagraca04@gmail.com>
2020-04-14 01:26:11 -0300

    First Commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,56 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionTDO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
+  // Retorna todos as transações e o balanço
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
+  // Faz o balanço total dos income e outcome
   public getBalance(): Balance {
-    // TODO
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  // Cria uma nova transação
+  public create({ title, value, type }: CreateTransactionTDO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (type === 'income') {
+      this.balance.income += value;
+    }
+    if (type === 'outcome') {
+      if (this.balance.total >= value) {
+        this.balance.outcome += value;
+      } else {
+        throw Error('Valor Estrapolado');
+      }
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
